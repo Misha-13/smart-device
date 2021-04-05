264f85e7a569273b2b093556b7aa5ca04283a914
Misha-13 <m.a.cherkasov@yandex.ru>
2021-04-05 23:16:02 +0300

    Добавляет ПП и скрипт номера телефона

--- build/js/main.js
+++ build/js/main.js
@@ -5,23 +5,42 @@
   var modalOpenButton = body.querySelector('.contact-info__button');
   var modalWindow = body.querySelector('.modal');
   var closeModal = modalWindow.querySelector('.modal__close-button');
+  var moadlUserName = modalWindow.querySelector('#name-modal');
+  var moadlUserPhone = modalWindow.querySelector('#phone-modal');
+  var moadlUserQuestion = modalWindow.querySelector('#question-modal');
 
   var setModalsClass = function (main, modal) {
     main.classList.toggle('page-body--modal');
     modal.classList.toggle('modal--close');
   };
 
+  var setStorageValue = function () {
+    localStorage.setItem('userName', moadlUserName.value);
+    localStorage.setItem('userPhone', moadlUserPhone.value);
+    localStorage.setItem('userQuestion', moadlUserQuestion.value);
+  };
+
+  var getStorageValue = function () {
+    moadlUserName.value = localStorage.getItem('userName');
+    moadlUserPhone.value = localStorage.getItem('userPhone');
+    moadlUserQuestion.value = localStorage.getItem('userQuestion');
+  };
+
   var setEvent = function () {
     modalOpenButton.addEventListener('click', function (evt) {
       evt.preventDefault();
       setModalsClass(body, modalWindow);
+      getStorageValue();
+      moadlUserName.focus();
     });
 
     closeModal.addEventListener('click', function () {
+      setStorageValue();
       setModalsClass(body, modalWindow);
     });
     window.addEventListener('click', function (evt) {
       if (evt.target === modalWindow) {
+        setStorageValue();
         setModalsClass(body, modalWindow);
       }
     });
@@ -85,5 +104,40 @@
 
 })();
 
+(function () {
+  var phoneInputs = document.querySelectorAll('.common-form__wrapper input[type = "tel"]');
+
+  var setPhoneEvents = function () {
+    phoneInputs.forEach(function (element) {
+      element.addEventListener('focus', function () {
+        if (!(element.value.includes('+7('))) {
+          element.value = '+7(';
+        }
+      });
+      element.addEventListener('keypress', function (evt) {
+        var str = element.value;
+        if (parseInt(evt.key, 10)) {
+          if (str.length === 6) {
+            element.value = str + ')';
+          }
+          if (str.length === 14) {
+            evt.returnValue = false;
+          }
+        } else {
+          evt.returnValue = false;
+        }
+      });
+    });
+  };
+
+  window.phone = {
+    setPhone: function () {
+      setPhoneEvents();
+    }
+  };
+
+})();
+
 window.modal.setModal();
 window.accordion.setAccordion();
+window.phone.setPhone();
